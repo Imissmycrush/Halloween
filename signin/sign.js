00383fa18e7cb28f66ef539ce4ba5da82526b93d
Imissmycrush <nguyentrongv333@gmail.com>
2024-10-27 17:29:43 +0700

    10
    10

---
+++ signin/sign.js
@@ -0,0 +1,45 @@
+
+// Showing or Toggling Menu
+
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
+
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    });
+}
+
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    });
+}
+
+//Clicking in one option, removes nav mobile
+
+const navLink = document.querySelectorAll('.nav-link');
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show-menu');
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+//Adding Atropos function
+
+const myAtropos = Atropos({
+    el: '.home-images',
+    shadow: false,
+    highlight: false,
+})
+
+//Shadowheader when scroll is higher than 50% view hight
+
+const shadowHeader = () => {
+    const header = document.getElementById('header');
+    this.scrollY >= 50 ? header.classList.add('shadow-header') : header.classList.remove('shadow-header');
+}
+window.addEventListener('scroll', shadowHeader);
