cf606736fbe1b157e7248d74995cd53c5374dfe6
Imissmycrush <nguyentrongv333@gmail.com>
2024-10-28 18:15:44 +0700

    Update sign.js

--- signin/sign.js
+++ signin/sign.js
@@ -43,8 +43,3 @@ const shadowHeader = () => {
     }
 }
 window.addEventListener('scroll', shadowHeader);
-
-const overlay = document.getElementById('overlay');
-document.addEventListener('mousemove', (e) => {
-    overlay.style.background = `radial-gradient(circle at ${e.pageX}px ${e.pageY}px, rgba(255, 255, 255, 0.3), rgba(0, 0, 0, 0.8) 150px)`;
-});
